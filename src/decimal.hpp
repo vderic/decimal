d2feecf9412faff2e29c7a1389ea2900714ee27f
eric <ericlam@vitessedata.com>
2023-10-24 16:34:39 +0100

    update

--- src/decimal.hpp
+++ src/decimal.hpp
@@ -1,41 +1,68 @@
 #pragma once
 
+#include <stdexcept>
 #include "basic_decimal.h"
 
-bool operator==(const decimal128_t &left, const decimal128_t &right) {
+
+inline bool operator==(const decimal128_t &left, const decimal128_t &right) {
   return dec128_cmpeq(left, right);
 }
 
-bool operator!=(const decimal128_t &left, const decimal128_t &right) {
+inline bool operator!=(const decimal128_t &left, const decimal128_t &right) {
   return dec128_cmpne(left, right);
 }
 
-bool operator<(const decimal128_t &left, const decimal128_t &right) {
+inline bool operator<(const decimal128_t &left, const decimal128_t &right) {
   return dec128_cmplt(left, right);
 }
 
-bool operator<=(const decimal128_t &left, const decimal128_t &right) {
+inline bool operator<=(const decimal128_t &left, const decimal128_t &right) {
   return dec128_cmple(left, right);
 }
-bool operator>(const decimal128_t &left, const decimal128_t &right) {
+
+inline bool operator>(const decimal128_t &left, const decimal128_t &right) {
   return dec128_cmpgt(left, right);
 }
-bool operator>=(const decimal128_t &left, const decimal128_t &right) {
+inline bool operator>=(const decimal128_t &left, const decimal128_t &right) {
   return dec128_cmpge(left, right);
 }
 
-#if 0
-ARROW_EXPORT BasicDecimal128 operator-(const BasicDecimal128& operand);
-ARROW_EXPORT BasicDecimal128 operator~(const BasicDecimal128& operand);
-ARROW_EXPORT BasicDecimal128 operator+(const BasicDecimal128& left,
-                                       const BasicDecimal128& right);
-ARROW_EXPORT BasicDecimal128 operator-(const BasicDecimal128& left,
-                                       const BasicDecimal128& right);
-ARROW_EXPORT BasicDecimal128 operator*(const BasicDecimal128& left,
-                                       const BasicDecimal128& right);
-ARROW_EXPORT BasicDecimal128 operator/(const BasicDecimal128& left,
-                                       const BasicDecimal128& right);
-ARROW_EXPORT BasicDecimal128 operator%(const BasicDecimal128& left,
-                                       const BasicDecimal128& right);
+inline decimal128_t operator-(const decimal128_t& operand) {
+	return dec128_negate(operand);
+}
 
-#endif
+inline decimal128_t operator~(const decimal128_t& operand) {
+	decimal128_t result = dec128_from_hilo(~dec128_high_bits(operand), ~dec128_low_bits(operand));
+	return result;
+}
+
+inline decimal128_t operator+(const decimal128_t& left, const decimal128_t& right) {
+	return dec128_sum(left, right);
+}
+
+inline decimal128_t operator-(const decimal128_t& left, const decimal128_t& right) {
+	return dec128_subtract(left, right);
+}
+
+inline decimal128_t operator*(const decimal128_t& left, const decimal128_t& right) {
+	return dec128_multiply(left, right);
+}
+
+inline decimal128_t operator/(const decimal128_t& left, const decimal128_t& right) {
+	decimal128_t remainder, result;
+	decimal_status_t s = dec128_divide(left, right, &result, &remainder);
+	if (s != DEC128_STATUS_SUCCESS) {
+		throw std::runtime_error("dec128_divide error");
+	}
+	return result;
+}
+
+inline decimal128_t operator%(const decimal128_t& left, const decimal128_t& right) {
+	decimal128_t remainder, result;
+	decimal_status_t s = dec128_divide(left, right, &result, &remainder);
+	if (s != DEC128_STATUS_SUCCESS) {
+		throw std::runtime_error("dec128_divide error");
+	}
+	return remainder;
+}
+
