5bc1a16d82f4695120ab7d680ed2d0814ae774b9
eric <ericlam@vitessedata.com>
2023-10-25 17:52:25 +0100

    cpp wrapper

--- src/decimal.hpp
+++ src/decimal.hpp
@@ -3,76 +3,142 @@
 #include "basic_decimal.h"
 #include <stdexcept>
 
+struct Decimal128 {
+  decimal128_t dec;
+
+  Decimal128() { dec = {0}; }
+
+  Decimal128(decimal128_t _dec) { dec = _dec; }
+
+  Decimal128(int64_t v) { dec = dec128_from_int64(v); }
+
+  Decimal128(int64_t hi, uint64_t lo) { dec = dec128_from_hilo(hi, lo); }
+
+  Decimal128(const uint8_t *bytes) { dec = dec128_from_pointer(bytes); }
+
+  static decimal_status_t FromString(const char *s, Decimal128 *out,
+                                     int32_t *precision, int32_t *scale) {
+    return dec128_from_string(s, &out->dec, precision, scale);
+  }
+
+  Decimal128 &operator+=(const Decimal128 &right) {
+    dec = dec128_sum(dec, right.dec);
+    return *this;
+  }
+
+  Decimal128 &operator-=(const Decimal128 &right) {
+    dec = dec128_subtract(dec, right.dec);
+    return *this;
+  }
+
+  Decimal128 &operator*=(const Decimal128 &right) {
+    dec = dec128_multiply(dec, right.dec);
+    return *this;
+  }
+
+  /* normalize to the same scale before divide */
+  Decimal128 &operator/=(const Decimal128 &right) {
+    decimal128_t result, remainder;
+    decimal_status_t s = dec128_divide(dec, right.dec, &result, &remainder);
+    if (s) {
+      throw std::runtime_error("dec128_divide failed");
+    }
+    dec = result;
+    return *this;
+  }
+
+  /* normalize to the same scale before divide */
+  Decimal128 &operator%=(const Decimal128 &right) {
+    decimal128_t result, remainder;
+    decimal_status_t s = dec128_divide(dec, right.dec, &result, &remainder);
+    if (s) {
+      throw std::runtime_error("dec128_divide failed");
+    }
+    dec = remainder;
+    return *this;
+  }
+
+  int64_t high_bits() { return dec128_high_bits(dec); }
+
+  uint64_t low_bits() { return dec128_low_bits(dec); }
+
+  Decimal128 IncreaseScaleBy(int32_t increase_by) {
+    return Decimal128(dec128_increase_scale_by(dec, increase_by));
+  }
+
+  Decimal128 ReduceScaleBy(int32_t reduce_by, bool round = true) {
+    return Decimal128(dec128_reduce_scale_by(dec, reduce_by, round));
+  }
+
+  void print(int precision, int scale) {
+    dec128_print(stdout, dec, precision, scale);
+  }
+};
+
 inline decimal_status_t dec128_from_string(const std::string &value,
                                            decimal128_t *out, int *precision,
                                            int *scale) {
   return dec128_from_string(value.c_str(), out, precision, scale);
 }
 
-inline bool operator==(const decimal128_t &left, const decimal128_t &right) {
-  return dec128_cmpeq(left, right);
+inline bool operator==(const Decimal128 &left, const Decimal128 &right) {
+  return dec128_cmpeq(left.dec, right.dec);
 }
 
-inline bool operator!=(const decimal128_t &left, const decimal128_t &right) {
-  return dec128_cmpne(left, right);
+inline bool operator!=(const Decimal128 &left, const Decimal128 &right) {
+  return dec128_cmpne(left.dec, right.dec);
 }
 
-inline bool operator<(const decimal128_t &left, const decimal128_t &right) {
-  return dec128_cmplt(left, right);
+inline bool operator<(const Decimal128 &left, const Decimal128 &right) {
+  return dec128_cmplt(left.dec, right.dec);
 }
 
-inline bool operator<=(const decimal128_t &left, const decimal128_t &right) {
-  return dec128_cmple(left, right);
+inline bool operator<=(const Decimal128 &left, const Decimal128 &right) {
+  return dec128_cmple(left.dec, right.dec);
 }
 
-inline bool operator>(const decimal128_t &left, const decimal128_t &right) {
-  return dec128_cmpgt(left, right);
+inline bool operator>(const Decimal128 &left, const Decimal128 &right) {
+  return dec128_cmpgt(left.dec, right.dec);
 }
-inline bool operator>=(const decimal128_t &left, const decimal128_t &right) {
-  return dec128_cmpge(left, right);
-}
-
-inline decimal128_t operator-(const decimal128_t &operand) {
-  return dec128_negate(operand);
+inline bool operator>=(const Decimal128 &left, const Decimal128 &right) {
+  return dec128_cmpge(left.dec, right.dec);
 }
 
-inline decimal128_t operator~(const decimal128_t &operand) {
-  decimal128_t result =
-      dec128_from_hilo(~dec128_high_bits(operand), ~dec128_low_bits(operand));
-  return result;
+inline Decimal128 operator-(const Decimal128 &operand) {
+  return Decimal128(dec128_negate(operand.dec));
 }
 
-inline decimal128_t operator+(const decimal128_t &left,
-                              const decimal128_t &right) {
-  return dec128_sum(left, right);
+inline Decimal128 operator~(const Decimal128 &operand) {
+  return Decimal128(~dec128_high_bits(operand.dec),
+                    ~dec128_low_bits(operand.dec));
 }
 
-inline decimal128_t operator-(const decimal128_t &left,
-                              const decimal128_t &right) {
-  return dec128_subtract(left, right);
+inline Decimal128 operator+(const Decimal128 &left, const Decimal128 &right) {
+  Decimal128 ret = left;
+  ret += right;
+  return ret;
 }
 
-inline decimal128_t operator*(const decimal128_t &left,
-                              const decimal128_t &right) {
-  return dec128_multiply(left, right);
+inline Decimal128 operator-(const Decimal128 &left, const Decimal128 &right) {
+  Decimal128 ret = left;
+  ret -= right;
+  return ret;
 }
 
-inline decimal128_t operator/(const decimal128_t &left,
-                              const decimal128_t &right) {
-  decimal128_t remainder, result;
-  decimal_status_t s = dec128_divide(left, right, &result, &remainder);
-  if (s != DEC128_STATUS_SUCCESS) {
-    throw std::runtime_error("dec128_divide error");
-  }
-  return result;
+inline Decimal128 operator*(const Decimal128 &left, const Decimal128 &right) {
+  Decimal128 ret = left;
+  ret *= right;
+  return ret;
 }
 
-inline decimal128_t operator%(const decimal128_t &left,
-                              const decimal128_t &right) {
-  decimal128_t remainder, result;
-  decimal_status_t s = dec128_divide(left, right, &result, &remainder);
-  if (s != DEC128_STATUS_SUCCESS) {
-    throw std::runtime_error("dec128_divide error");
-  }
-  return remainder;
+inline Decimal128 operator/(const Decimal128 &left, const Decimal128 &right) {
+  Decimal128 ret = left;
+  ret /= right;
+  return ret;
+}
+
+inline Decimal128 operator%(const Decimal128 &left, const Decimal128 &right) {
+  Decimal128 ret = left;
+  ret %= right;
+  return ret;
 }
--- test/xdec2.cpp
+++ test/xdec2.cpp
@@ -2,26 +2,42 @@
 
 int main() {
 
-  decimal128_t d1 = dec128_from_int64(12345567);
-  decimal128_t d2;
+  Decimal128 d1(100);
+  d1.print(38, 2);
+  Decimal128 d2(20);
+
+  d1 /= d2;
+
+  d1.print(38, 2);
+
+  d1 = d1.IncreaseScaleBy(3);
+
+  d1.print(38, 2);
+
+  printf("size of deciml128 = %lu\n", sizeof(Decimal128));
+
+  Decimal128 d3(1234567);
+  Decimal128 d4;
   int precision, scale;
 
-  decimal_status_t s = dec128_from_string("12345567", &d2, &precision, &scale);
+  decimal_status_t s =
+      Decimal128::FromString("1234567", &d4, &precision, &scale);
   if (s) {
     printf("ERROR\n");
     return 1;
   }
 
-  if (d1 == d2) {
+  if (d3 == d4) {
     printf("OK\n");
   } else {
     printf("FAILED\n");
   }
-  dec128_print(stdout, d1, 38, 0);
-  dec128_print(stdout, d2, 38, 0);
 
-  decimal128_t d3 = d1 + d2;
-  dec128_print(stdout, d3, 38, 0);
+  d3.print(38, 0);
+  d4.print(38, 0);
+
+  Decimal128 d5 = d1 + d2;
+  d5.print(38, 0);
 
   return 0;
 }
